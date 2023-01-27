ccf195a356dacf69a2edd3667070b9763f6d44b3
mattllama12 <77423061+mattllama12@users.noreply.github.com>
2023-01-26 20:18:27 -0500

    Add files via upload
    Added Executioner Blade and Hiramekarei with sword effects

---
+++ Code/ModSources/NarutoTerraria/Items/Weapons/Hiramekarei.cs
@@ -0,0 +1,51 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+
+namespace NarutoTerraria.Items.Weapons
+{
+    public class Hiramekarei : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hiramekarei"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+            Tooltip.SetDefault("Hiramekarei");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 20;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 50;
+            Item.useAnimation = 50;
+            Item.useStyle = 1;
+            Item.knockBack = 6;
+            //Item.shoot = ProjectileID.Flames;
+            //Item.shootSpeed = 5f;
+            Item.value = 10000;
+            Item.rare = 8;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+
+        public override bool? UseItem(Player player) //Edit Dust ID for a different Effect
+        {
+            if (Main.rand.Next(3) == 0)
+                Dust.NewDust(new Vector2(player.position.X, player.position.Y), player.Hitbox.Width, player.Hitbox.Height, DustID.GrassBlades);
+            return base.UseItem(player);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+
+    }
+}
